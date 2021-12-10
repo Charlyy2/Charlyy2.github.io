d7e9c071e9ad9624fdd2063d280e3d48a5e55a6c
Charlyy1 <charlybrav127@gmail.com>
2021-12-09 22:35:09 -0300

    lalala

--- js/database/usuarios.js
+++ js/database/usuarios.js
@@ -151,6 +151,7 @@ function buscarPalabras(frase, palabra) {
 
 
 function agregarId() {
+    
     //genera el ID procedural
     let usuarioId;
     let arrayGuardado = JSON.parse(localStorage.getItem("Usuarios"));
